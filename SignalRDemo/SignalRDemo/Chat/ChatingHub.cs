c75a1f843ee48c7c019514e469931525b7a7ec3a
Ashok Patel <88651476+ashokadk457@users.noreply.github.com>
2024-03-11 17:28:26 +0530

    Add files via upload

---
+++ SignalRDemo/SignalRDemo/Chat/ChatingHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace SignalRDemo.Chat
+{
+    [AllowAnonymous]
+    public class ChatingHub : Hub
+    {
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Debug.WriteLine("Client disconnected: " + Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+        public override Task OnConnectedAsync()
+        {
+            Debug.WriteLine("Client connected: " + Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+        //Create Group for each user to chat sepeartely
+        public void SetUserChatGroup(string userChatId)
+        {
+            var id = Context.ConnectionId;
+            Debug.WriteLine($"Client {id} added to group " + userChatId);
+            Groups.AddToGroupAsync(Context.ConnectionId, userChatId);
+        }
+        //Send message to user Group
+        public async Task SendMessageToGroup(string senderChatId,string senderName, string receiverChatId, string message)
+        {
+            await Clients.Group(receiverChatId).SendAsync("ReceiveMessage", senderChatId, senderName, receiverChatId, message);
+        }
+    }
+}
---
+++ SignalRDemo/SignalRDemo/Program.cs
@@ -0,0 +1,36 @@
+using SignalRDemo.Chat;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<ChatingHub>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .SetIsOriginAllowed((host) => true)
+    .AllowCredentials()
+  );
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<ChatingHub>("/ChatingHub");
+app.Run();
